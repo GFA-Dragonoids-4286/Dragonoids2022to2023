65e0e2ca15bfe96888403eeb1cdf95ba26644c6f
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2022-11-04 18:12:52 -0400

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheels.java
@@ -97,6 +97,36 @@ public class MecanumWheels extends OpMode
      */
     @Override
     public void loop() {
+        double x = gamepad1.left_stick_x
+        double y = gamepad1.left_stick_y
+        double theta = Math.atan(y/x);
+        if (x > 0 && y > 0){
+            theta = theta;
+        } else if (x < 0 && y > 0){
+            theta -= 180;
+        } else if (x > 0 && y < 0){
+            theta += 180;
+        } else if (x < 0 && y < 0){
+            theta = -theta;
+        }
+
+
+
+        /*
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        leftFront.setPower(v1);
+        rightFront.setPower(v2);
+        leftBack.setPower(v3);
+        rightBack.setPower(v4);
+        */
+
     }
 
     /*
