2dc46dccd6448483fec6ca791ffc6f9ca70510ae
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2022-11-05 10:05:15 -0400

    power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheels.java
@@ -129,20 +129,10 @@ public class MecanumWheels extends OpMode
             rightBackPower /= power + turn;
         }
 
-        /*
-        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
-        final double v1 = r * Math.cos(robotAngle) + rightX;
-        final double v2 = r * Math.sin(robotAngle) - rightX;
-        final double v3 = r * Math.sin(robotAngle) + rightX;
-        final double v4 = r * Math.cos(robotAngle) - rightX;
-
-        leftFront.setPower(v1);
-        rightFront.setPower(v2);
-        leftBack.setPower(v3);
-        rightBack.setPower(v4);
-        */
+        leftFront.setPower(leftFrontPower);
+        leftBack.setPower(leftBackPower);
+        rightFront.setPower(rightFrontPower);
+        rightBack.setPower(rightBackPower);
 
     }
 
