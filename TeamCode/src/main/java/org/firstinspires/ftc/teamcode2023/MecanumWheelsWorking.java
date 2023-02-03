02322dcf31ec30cfec433d2d721426e058e096ed
Brian Twitchell <pigwarriors1@gmail.com>
2023-02-03 17:34:00 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheelsWorking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode2023/MecanumWheelsWorking.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -46,15 +17,6 @@ import java.util.Math;
  * The names of OpModes appear on the menu of the FTC Driver Station.
  * When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two
- * wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code
- * folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver
- * Station OpMode list
  */
 
 @TeleOp(name = "MechyWheels Work", group = "Iterative Opmode")
@@ -90,7 +52,8 @@ public class MecanumWheelsWorking extends OpMode {
         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
-
+        
+        //checks to make sure that the max is not exeded the most motor power
         if (max > 1.0) {
             leftFrontPower /= max;
             rightFrontPower /= max;
@@ -118,9 +81,11 @@ public class MecanumWheelsWorking extends OpMode {
     }
 
     public void driveClaw(boolean open, boolean close) {
+        //sets open and close values
         double openValue = 0.75;
         double closeValue = 0.1;
 
+        //checks buttons and opens or closes claw
         if(open) {
             claw.setPosition(openValue);
         } 
@@ -131,12 +96,14 @@ public class MecanumWheelsWorking extends OpMode {
     }
 
     public void initWheels() {
+        //gets Wheels
         leftFront = hardwareMap.get(DcMotor.class, "flm");
         leftBack = hardwareMap.get(DcMotor.class, "blm");
         rightFront = hardwareMap.get(DcMotor.class, "frm");
         rightBack = hardwareMap.get(DcMotor.class, "brm");
         slider = hardwareMap.get(DcMotor.class, "Slider")
-
+        
+        //Sets Direction of Wheels
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
@@ -147,6 +114,7 @@ public class MecanumWheelsWorking extends OpMode {
 
     public void initSlider() {
         slider = hardwareMap.get(Servo.class, "slider");
+
     }
 
     public void initArm() {
@@ -161,8 +129,12 @@ public class MecanumWheelsWorking extends OpMode {
           "Beep Boop.",
           "Taking Over The World",
           "About to Win The Contest",
-          "Don't get stuck"
+          "Don't get stuck",
+          "I Bust Nuts",
+          "Somthings Broken...",
+          "Fuck Mrs. Smith"
         };
+
         telemetry.addData("Status", possibleSayings[(int) (Math.random() * possibleSayings.length)]);
     }
     
@@ -212,6 +184,18 @@ public class MecanumWheelsWorking extends OpMode {
      */
     @Override
     public void stop() {
+        
+        // Create an Array of Possible Sayings the Robot can Say When it Shuts Down
+        String[] possibleSayings =
+        new String[] {
+          "Goodbye",
+          "Sweet Dreams",
+          "Boop Beep.",
+          "No Longer Taking Over The World",
+          "Thinking About Our Win",
+          "Preparing for the Post-Win Party"
+        };
+        telemetry.addData("Status", possibleSayings[(int) (Math.random() * possibleSayings.length)]);
     }
 
 }
